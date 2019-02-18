ea262c55bc1f64a3fcbcd2b8f6edb50e1ae7add8
pappgeri096 <pappgergo096@gmail.com>
2019-02-18 11:19:18 +0100

    roles guard implemented

--- src/app/services/auth/roles-guard/roles-guard.service.ts
+++ src/app/services/auth/roles-guard/roles-guard.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
+import {AuthService} from '../auth.service';
+import {TokenStorageService} from '../token-storage/token-storage.service';
+import decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RolesGuardService implements CanActivate{
 
-  constructor() { }
+  constructor(private auth: AuthService, private tokenStorage: TokenStorageService  , private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return undefined;
+    const expectedRole = route.data.expectedRole;
+    const token = this.tokenStorage.getToken();
+    const decodeToken = decode(token);
+
+    if (
+      !this.auth.isLogedIn() ||
+      decodeToken.role !== expectedRole
+    ) {
+      this.router.navigate(['/lodgings']);
+      return false;
+    }
+    return true;
   }
 }

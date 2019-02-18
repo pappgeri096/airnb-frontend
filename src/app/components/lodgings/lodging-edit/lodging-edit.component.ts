5efe5723a02b68b53bec157184887902c573be21
pappgeri096 <pappgergo096@gmail.com>
2019-02-18 14:09:26 +0100

    edit form works

--- src/app/components/lodgings/lodging-edit/lodging-edit.component.ts
+++ src/app/components/lodgings/lodging-edit/lodging-edit.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Lodging} from '../../../models/lodging.model';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {LodgingsService} from '../../../services/lodgings/lodgings.service';
-import {NgForm} from '@angular/forms';
+import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
+import {Address} from '../../../models/address';
 
 @Component({
   selector: 'app-lodging-edit',
@@ -13,16 +14,38 @@ export class LodgingEditComponent implements OnInit {
 
   private id: number;
   private _lodging: Lodging;
+  editForm: FormGroup;
+  error: false;
 
   constructor(private route: ActivatedRoute, private lodgingsService: LodgingsService, private router: Router) { }
 
   ngOnInit() {
+    this.editForm = new FormGroup({
+      'lodging_name': new FormControl(null, Validators.required),
+      'lodging_type': new FormControl(null, Validators.required),
+      'daily_price': new FormControl(null, Validators.required),
+      'electricity_bill': new FormControl(null, Validators.required),
+      'gas_bill': new FormControl(null, Validators.required),
+      'telecommunication_bill': new FormControl(null, Validators.required),
+      'cleaning_cost': new FormControl(null, Validators.required),
+      'address': new FormGroup({
+        'country': new FormControl(null, Validators.required),
+        'city': new FormControl(null, Validators.required),
+        'zip_code': new FormControl(null, Validators.required),
+        'address': new FormControl(null, Validators.required),
+      }),
+    });
+
     this.route.params.
     subscribe(
       (params: Params) => {
         this.id = +params['id'];
+        this.setLodgingsValues(this.id);
       }
     );
+
+
+
   }
 
 
@@ -30,9 +53,55 @@ export class LodgingEditComponent implements OnInit {
     return this._lodging;
   }
 
-  onSubmit(form: NgForm){
+  onSubmit(){
 
-    this.router.navigate(['lodgings']);
+    const data = this.editForm.value;
+
+    const lodging: Lodging = new Lodging();
+    lodging.name = data['lodging_name'];
+    lodging.lodgingsType = data['lodging_type'];
+    lodging.pricePerDay = data['daily_price'];
+    lodging.electricityBill = data['electricity_bill'];
+    lodging.gasBill = data['gas_bill'];
+    lodging.telecommunicationBill = data['telecommunication_bill'];
+    lodging.cleaningCost = data['cleaning_cost'];
+    lodging.fullAddress = new Address(data['address']['country'], data['address']['city'], data['address']['zip_code'], data['address']['address']);
+
+    this.lodgingsService.updateLodgings(lodging).subscribe(
+      (response) => {
+        console.log(response);
+        this.router.navigate(['/lodgings']);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
+
+  private setLodgingsValues(id: number) {
+    this.lodgingsService.getLodgingsById(this.id).subscribe(
+      (lodging: Lodging) => {
+        this.editLodgings(lodging);
+      }
+    );
+  }
+
+  private editLodgings(lodging: Lodging) {
+    this.editForm.setValue({
+      'lodging_name': lodging.name,
+      'lodging_type': lodging.lodgingsType,
+      'daily_price': lodging.pricePerDay,
+      'electricity_bill': lodging.electricityBill,
+      'gas_bill': lodging.gasBill,
+      'telecommunication_bill': lodging.telecommunicationBill,
+      'cleaning_cost': lodging.cleaningCost,
+      'address': {
+        'country': lodging.fullAddress.address,
+        'city': lodging.fullAddress.city,
+        'zip_code': lodging.fullAddress.zipCode,
+        'address': lodging.fullAddress.address,
+    }
+  });
+}
 
 }

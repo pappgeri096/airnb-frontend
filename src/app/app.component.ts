7b4562292767b0fe277218819831771edef8ecf2
pappgeri096 <pappgergo096@gmail.com>
2019-03-05 09:48:12 +0100

    bootstrap 3 updated to version 4 dashboard template added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import {UsersService} from './services/users/users.service';
+import {AuthService} from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+  constructor(private userService: UsersService, private auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  logedIn(){
+    return this.auth.isLogedIn();
+  }
 
 }

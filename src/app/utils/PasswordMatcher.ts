c0c6af890b35df4b4987ddc6645bec2310d128a2
pappgeri096 <pappgergo096@gmail.com>
2019-03-06 15:18:32 +0100

    password conformation message deleted

--- src/app/utils/PasswordMatcher.ts
+++ src/app/utils/PasswordMatcher.ts
@@ -4,9 +4,9 @@ export class PasswordMatcher {
   static MatchPassword(AC: AbstractControl) {
     let password = AC.get('password').value; // to get value in input tag
     let confirmPassword = AC.get('password_confirmation').value; // to get value in input tag
-    if(password != confirmPassword) {
+    if(password !== confirmPassword) {
       console.log(AC);
-      return {MatchPassword: false};
+      AC.get('confirmPassword').setErrors( {MatchPassword: true});
     } else {
       console.log('true');
       return null;
